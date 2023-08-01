f0dafa7ce52f9ea36889fdd579a0e0a628ad9be9
Rekha <rekharajan@xminds.com>
2023-08-01 11:40:14 +0530

    Update program-3.js

--- program-3.js
+++ program-3.js
@@ -3,23 +3,41 @@ const person = {
     Age: 40,
     Cars: [
         {
-        name: "ford",
-        models: ["mustang", "Fiest", "Mustang"],
+            name: "ford",
+            models: ["mustang", "Fiest", "Mustang"],
         },
         {
-        Name: "BMW",
-        models: [320, 'x3', 'x5'],
+            Name: "BMW",
+            models: [320, 'x3', 'x5'],
         },
         {
-        Name: "fiat",
-        models: [500, "panda"],
+            Name: "fiat",
+            models: [500, "panda"],
         },
     ]
 };
-for (let [key, value] of Object.entries(person)) {
-    console.log(key, value);
+
+
+function displayObject(obj) {
+    for (const key in obj) {
+        const value = obj[key];
+        if (Array.isArray(value)) {
+            console.log(key + ':');
+            value.forEach((element) => {
+                if (typeof element === 'object' && element !== null) {
+                    displayObject(element);
+                } else {
+                    console.log(element);
+                }
+            });
+        } else if (typeof value === 'object' && value !== null) {
+            for (const key2 in value) {
+                console.log(key2 + ': ' + value[key2]);
+            }
+        } else {
+            console.log(key + ': ' + value);
+        }
+    };
 }
 
-for (let key in person) {
-    console.log(key, person[key]);
-}
+displayObject(person);
