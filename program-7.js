9137ce46145207633d98df493b447d580536f00c
Rekha <rekharajan@xminds.com>
2023-08-02 11:56:39 +0530

    Update program-7.js

--- program-7.js
+++ program-7.js
@@ -1,2 +1,13 @@
-const pickNameStartWithJ = names => names.filter(name => name.startsWith('j'));
-console.log(pickNameStartWithJ(['ammu','bichu','cichu','jimmy','john']));
+/**Start with letter j */
+const pickNameStartWithJ = names => names.filter(name => name.startsWith('j')).map((element) => element.toUpperCase());
+console.log('Name Start with J :' + pickNameStartWithJ(['ammu','bichu','cichu','jimmy','john']));
+
+/****unique result */
+const myArray = ["Vishnu","Akhil","Aswin","Pooja","Vishnu","Ananthan","Jith","Jith","Pooja","Aswin"];
+const uniqueArray = myArray.reduce((oldresult, item) => {
+  if (!oldresult.includes(item)) {
+    oldresult.push(item);
+  }
+  return oldresult;
+}, []);
+console.log('Name Start with J :' + uniqueArray);
